b10ebf5fa2b60e019ccb8ecb372a0c6e91f33475
fluffy <hgoziev001@gmail.com>
2020-12-19 15:08:56 +0500

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const payment = require("./payment");
+const path = require("path");
 const contact = require("./contact");
 const app = express();
 const port = process.env.PORT || 5000;
