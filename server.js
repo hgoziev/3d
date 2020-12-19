0e8e1c93561afef607458daf42fe4db3d19ded95
fluffy <hgoziev001@gmail.com>
2020-12-19 15:09:46 +0500

    make it better

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use("/payment", payment);
 app.use("/contact", contact);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 app.listen(port, () => {
